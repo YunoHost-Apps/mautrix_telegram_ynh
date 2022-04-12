3b5ce11787ffe36df22cededc52d7c993882133e
Dante <thardev@users.noreply.github.com>
2022-04-12 17:25:33 +0100

    Configure end-to-bridge encryption

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,7 @@
 # dependencies used by the app
 # HACK zlib1g-dev libjpeg-dev (libwebp-dev optional but necessary for stickers) are necessary to compile / install pillow
 pkg_dependencies="postgresql python3 zlib1g-dev libjpeg-dev libwebp-dev python3-venv libpq-dev"
+pkg_dependencies_e2be="libolm-dev"
 
 #=================================================
 # PERSONAL HELPERS
